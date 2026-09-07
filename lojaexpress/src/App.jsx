546e2e66c704e2636aeda25b8a960cce02cf4f5b
VitorPrates <vitoraaprates@gmail.com>
2026-09-07 14:24:56 -0300

    ajustando design dos cards dos produtos

--- lojaexpress/src/App.jsx
+++ lojaexpress/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { useState, useEffect } from 'react'
-import  CardItem  from "./Componentes/CardItem.jsx"
 import * as Icons from "react-bootstrap-icons"
+import CardItem from './Componentes/CardItem.jsx'
 import Header from './Componentes/Header.jsx'
 
 
@@ -9,7 +9,7 @@ function App() {
   return (
     <div>
       <Header/>
-      <form action="" className='w-[95%] m-auto max-w-275 p-3'>
+      <form action="" method='POST' className='w-[95%] m-auto max-w-275 p-3'>
         <nav className='grid grid-cols-5 grid-rows-2 justify-center items-center *:border *:p-1 *:flex *:items-center *:gap-1'>
           <p> <Icons.Image/> Imagem</p>
           <p> <Icons.Box/> Produto</p>
@@ -23,7 +23,10 @@ function App() {
 
           <button className='col-start-5 row-start-1 row-end-3 '> <Icons.PlusLg/> Adicionar</button>
         </nav>
-        <CardItem/>
+        <div className='flex flex-col p-5 border gap-1'>
+          <CardItem/>
+          <CardItem/>
+        </div>
       </form>
     </div>
   )
--- lojaexpress/src/Componentes/CardItem.jsx
+++ lojaexpress/src/Componentes/CardItem.jsx
@@ -1,8 +1,19 @@
-export default function CardItem()
+import nindentificado from "../../public/n-identificado.png"
+import * as Icons from "react-bootstrap-icons"
+
+
+export default function CardItem({img,nome,quantidade,preco})
 {
     return(
-        <div className="flex border">
-            <h3>produtos</h3>
+        <div className="border grid grid-cols-[1fr_auto_repeat(6,1fr)] items-center justify-center text-center *:m-auto">
+            <img src={img? img : nindentificado} width={50} alt="" />
+            <p>{nome? nome : "Produto grande do "}</p>
+            <p>{quantidade? quantidade : "0"}</p>
+            <span ><Icons.X/></span>
+            <p>{preco? preco : "R$ 00,00"}</p>
+            <span>=</span>
+            <p>{quantidade && preco? `R$ ${quantidade*preco}` : `${(0).toLocaleString('pt-BR', {style: 'currency',currency: 'BRL'})}`}</p>
+            <span className="bg-gray-300 w-fit p-2 rounded-2xl text-red-600"><Icons.Trash/></span>
         </div>
     )
 }
