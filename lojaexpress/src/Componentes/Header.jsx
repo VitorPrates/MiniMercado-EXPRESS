693ea4b5d405506c7bf5c050f6ce6ff0254dceba
VitorPrates <vitoraaprates@gmail.com>
2026-09-07 17:52:22 -0300

    Personalizando header

--- lojaexpress/src/Componentes/Header.jsx
+++ lojaexpress/src/Componentes/Header.jsx
@@ -1,8 +1,8 @@
 export default function Header()
 {
     return(
-        <header className="text-center p-5 border-b-blue-950 border-b-2" >
-            <h1 className="font-bold" >Mercadin Vendi Mais</h1>
+        <header className="text-center p-5 bg-blue-800 border-b-blue-950 border-b-2" >
+            <h1 className="font-bold text-white text-lg" >Mercadin Vendi Mais</h1>
         </header>
     )
 }
