b2a0e8b483f8f8816022bdce5ae17a4934b03a94
VitorPrates <vitoraaprates@gmail.com>
2026-09-08 21:03:44 -0300

    Arrumando código para começar estilização

--- lojaexpress/src/Componentes/FormAtualizador.jsx
+++ lojaexpress/src/Componentes/FormAtualizador.jsx
@@ -5,16 +5,7 @@ import nindentificado from "../../public/n-identificado.png";
 import * as Icons from "react-bootstrap-icons";
 
 
-export default function FormAtualizador({
-    id,
-    img,
-    nome,
-    quantidade,
-    preco,
-    onUpdate,
-    cancelar
-}) {
-
+export default function FormAtualizador({id,img,nome,quantidade,preco,onUpdate,cancelar}) {
     const [qtd, setQtd] = useState(quantidade ?? "");
     const [prc, setPrc] = useState(
         preco ? preco / 100 : ""
@@ -22,13 +13,9 @@ export default function FormAtualizador({
     const [novoNome, setNovoNome] = useState(nome ?? "");
 
     const handleSubmit = async (e) => {
-
         e.preventDefault();
-
         const formData = new FormData(e.target);
-
         try {
-
             const response = await fetch(
                 `http://localhost:5000/produtos/${id}`,
                 {
@@ -36,19 +23,14 @@ export default function FormAtualizador({
                     body: formData
                 }
             );
-
             const produto = await response.json();
-
             if (!response.ok) {
                 throw new Error(produto.error);
             }
-
             // Envia o produto atualizado para o App
             onUpdate(produto);
-
             // Fecha o formulário
             cancelar();
-
         } catch (error) {
 
             console.error(
@@ -58,192 +40,34 @@ export default function FormAtualizador({
 
         }
     };
-
-
     return (
-
-        <form
-            onSubmit={handleSubmit}
-            className="
-                border
-                w-[95%]
-                m-auto
-                mt-2.5
-                p-1
-                grid
-                grid-cols-[1fr_30%_15%_1fr_15%_repeat(4,1fr)]
-                items-center
-                justify-center
-                text-center
-                *:m-auto
-            "
-        >
-
-            {/* IMAGEM */}
-
-            <p className="
-                flex
-                flex-col
-                text-center
-                justify-center
-                items-center
-            ">
-
-                <img
-                    src={
-                        img
-                            ? `http://localhost:5000${img}`
-                            : nindentificado
-                    }
-                    width={50}
-                    alt=""
-                />
-
-                <input
-                    className="w-full"
-                    type="file"
-                    accept="image/*"
-                    name="imagem"
-                />
-
+        <form onSubmit={handleSubmit}className="border w-[95%] m-auto mt-2.5 p-1 grid grid-cols-[1fr_30%_15%_1fr_15%_repeat(4,1fr)] items-center justify-center text-center *:m-auto">
+            <p className=" flex flex-col text-center justify-center items-center ">
+                <img src={ img ? `http://localhost:5000${img}` : nindentificado } width={50} alt="" />
+                <input className="w-full"type="file"accept="image/*"name="imagem"/>
             </p>
-
-
-            {/* NOME */}
-
             <p className="flex flex-col">
-
                 {nome || "Nome do produto"}
-
-                <input
-                    className="border pl-1"
-                    type="text"
-                    name="nome"
-                    value={novoNome}
-                    onChange={(e) =>
-                        setNovoNome(e.target.value)
-                    }
-                />
-
+                <input className="border pl-1" type="text" name="nome" value={novoNome} onChange={(e) => setNovoNome(e.target.value) } />
             </p>
-
-
-            {/* QUANTIDADE */}
-
             <span className="flex flex-col w-full">
-
                 Quantidade
-
-                <input
-                    className="border text-center"
-                    type="number"
-                    name="quantidade"
-                    min={1}
-                    value={qtd}
-                    onChange={(e) =>
-                        setQtd(e.target.value)
-                    }
-                />
-
+                <input className="border text-center" type="number" name="quantidade" min={1} value={qtd} onChange={(e) => setQtd(e.target.value) } />
             </span>
-
-
             <span>
                 <Icons.X />
             </span>
-
-
-            {/* PREÇO */}
-
             <p className="w-full">
-
-                R$
-
-                <input
-                    type="number"
-                    name="preco"
-                    min={0.01}
-                    step={0.01}
-                    value={prc}
-                    className="border w-full text-center"
-                    onChange={(e) =>
-                        setPrc(e.target.value)
-                    }
-                />
-
+                R$<input type="number" name="preco" min={0.01} step={0.01} value={prc} className="border w-full text-center" onChange={(e) => setPrc(e.target.value) } />
             </p>
-
-
-            <span>
-                =
-            </span>
-
-
-            {/* TOTAL DO ITEM */}
-
-            <p>
-
-                {qtd && prc
-
-                    ? (
-                        Number(qtd) *
-                        Number(prc)
-                    ).toLocaleString(
-                        "pt-BR",
-                        {
-                            style: "currency",
-                            currency: "BRL"
-                        }
-                    )
-
-                    : "R$ 0,00"
-                }
-
-            </p>
-
-
-            {/* SALVAR */}
-
-            <div className="
-                flex
-                items-center
-                border-green-500
-                bg-green-300
-                border
-                w-fit
-                p-2
-                rounded-2xl
-                text-green-800
-            ">
-
-                <button
-                    type="submit"
-                    className="cursor-pointer"
-                >
+            <span>=</span>
+            <p>{qtd && prc ? ( Number(qtd) * Number(prc) ).toLocaleString( "pt-BR", { style: "currency", currency: "BRL" } ) : "R$ 0,00" }</p>
+            <div className="flex items-center border-green-500 bg-green-300 border w-fit p-2 rounded-2xl text-green-800 ">
+                <button type="submit" className="cursor-pointer">
                     <Icons.Check />
                 </button>
-
             </div>
-
-
-            {/* CANCELAR */}
-
-            <button
-                type="button"
-                className="
-                    cursor-pointer
-                    flex
-                    items-center
-                    border-red-500
-                    bg-red-300
-                    border
-                    w-fit
-                    p-2
-                    rounded-2xl
-                  text-red-800"onClick={cancelar}>
-                <Icons.X />
-            </button>
-
+           <button type="button" className=" cursor-pointer flex items-center border-red-500 bg-red-300 border w-fit p-2 rounded-2xl text-red-800"onClick={cancelar}> <Icons.X /> </button>
         </form>
 
     );
